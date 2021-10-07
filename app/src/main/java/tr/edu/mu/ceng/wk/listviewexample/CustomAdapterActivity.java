a71185b3f2b757f4e564930a8d09be85e952d0ea
U190709723 <190709723@posta.mu.edu.tr>
2021-10-07 22:14:27 +0300

    MAD

--- app/src/main/java/tr/edu/mu/ceng/wk/listviewexample/CustomAdapterActivity.java
+++ app/src/main/java/tr/edu/mu/ceng/wk/listviewexample/CustomAdapterActivity.java
@@ -2,12 +2,32 @@ package tr.edu.mu.ceng.wk.listviewexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomAdapterActivity extends AppCompatActivity {
+
+    final List<Animal> animals = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_adapter);
+
+        animals.add(new Animal("Cat", R.mipmap.ic_animal_foreground));
+        animals.add(new Animal("Dog", R.mipmap.ic_animal_foreground));
+        animals.add(new Animal("Elephant", R.mipmap.ic_animal_foreground));
+        animals.add(new Animal("Tiger", R.mipmap.ic_animal_foreground));
+        animals.add(new Animal("Lion", R.mipmap.ic_animal_foreground));
+        animals.add(new Animal("Jaguar", R.mipmap.ic_animal_foreground));
+        animals.add(new Animal("Bear", R.mipmap.ic_animal_foreground));
+
+        ListView listView = findViewById(R.id.listview);
+        AnimalAdapter adapter = new AnimalAdapter(animals, this);
+
+        listView.setAdapter(adapter);
+
     }
 }
